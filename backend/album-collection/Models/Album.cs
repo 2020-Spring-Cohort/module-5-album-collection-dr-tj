aead2c425fcc92138974ac8a9a8a224168e9feca
Jason <JSilvis030592@yahoo.com>
2020-03-11 13:13:33 -0400

    Added properties to Album.cs

--- backend/album-collection/Models/Album.cs
+++ backend/album-collection/Models/Album.cs
@@ -5,7 +5,14 @@ using System.Threading.Tasks;
 
 namespace album_collection.Models
 {
-    public class album
+    public class Album
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string RecordLabel { get; set; }
+        public string Image { get; set; }
+
+        //Artist Obj
+        //Song List
     }
 }
