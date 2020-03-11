bb3941dd173da40172c397c6bc9cafeba17d805c
Jason <JSilvis030592@yahoo.com>
2020-03-11 14:06:39 -0400

    Added properties for Song and artist model

--- backend/album-collection/Models/Artist.cs
+++ backend/album-collection/Models/Artist.cs
@@ -7,5 +7,11 @@ namespace album_collection.Models
 {
     public class Artist
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public string Genre { get; set; }
+
+        //List of Albums
     }
 }
--- backend/album-collection/Models/Song.cs
+++ backend/album-collection/Models/Song.cs
@@ -7,5 +7,10 @@ namespace album_collection.Models
 {
     public class Song
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+
+        //Link
     }
 }
