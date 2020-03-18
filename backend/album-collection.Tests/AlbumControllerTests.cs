d4a6ede0b64989972802113ceb9e38a87993ffc6
Dan Rasmussen <drasmussen49@gmail.com>
2020-03-18 14:46:59 -0400

    Added AlbumControllerTests class

---
+++ backend/album-collection.Tests/AlbumControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using album_collection.Controllers;
+using album_collection.Models;
+using album_collection.Repositories;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+
+namespace album_collection.Tests
+{
+    class AlbumControllerTests
+    {
+        private AlbumController underTest;
+        IRepository<Album> albumRepo;
+
+        public AlbumControllerTests()
+        {
+            albumRepo = Substitute.For<IRepository<Album>>();
+            underTest = new AlbumController(albumRepo);
+        }
+    }
+}
