8864429c3bc655309d84c29c6637331acb8399c9
TShaw516 <TShaw516@gmail.com>
2020-03-18 10:07:28 -0400

    added artistcontroller tests

--- backend/album-collection.Tests/ArtistControllerTests.cs
+++ backend/album-collection.Tests/ArtistControllerTests.cs
@@ -1,3 +1,7 @@
+using album_collection.Controllers;
+using album_collection.Models;
+using album_collection.Repositories;
+using NSubstitute;
 using System;
 using Xunit;
 
@@ -5,6 +9,15 @@ namespace album_collection.Tests
 {
     public class ArtistControllerTests
     {
+        private ArtistController underTest;
+        IRepository<Artist> artistRepo;
+
+        public ArtistControllerTests()
+        {
+            artistRepo = Substitute.For<IRepository<Artist>>();
+            underTest = new ArtistController(artistRepo);
+        }
+
         [Fact]
         public void Test1()
         {
