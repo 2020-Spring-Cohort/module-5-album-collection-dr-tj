39288b011982654b7f330ebed6dca3d6ce9f5f4c
Jason <JSilvis030592@yahoo.com>
2020-03-19 11:00:50 -0400

    Added first migration

---
+++ backend/album-collection/Migrations/20200319145746_InitialSeedDataMigration.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace album_collection.Migrations
+{
+    public partial class InitialSeedDataMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Genre = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    RecordLabel = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    ArtistId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Albums_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Duration = table.Column<string>(nullable: true),
+                    AlbumId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Songs_Albums_AlbumId",
+                        column: x => x.AlbumId,
+                        principalTable: "Albums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Artists",
+                columns: new[] { "Id", "Genre", "Image", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Rock", "", "Rush" },
+                    { 2, "Rock", "", "Queen" },
+                    { 3, "Pop", "", "Ace of Base" },
+                    { 4, "Ska", "", "Reel Big Fish" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Albums",
+                columns: new[] { "Id", "ArtistId", "Image", "RecordLabel", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "", "Anthem", "Moving Pictures" },
+                    { 2, 2, "", "EMI Records", "A Night at the Opera" },
+                    { 3, 3, "", "Arista", "The Sign" },
+                    { 4, 4, "", "Mojo", "Turn the Radio Off" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Songs",
+                columns: new[] { "Id", "AlbumId", "Duration", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "4:34", "Tom Sawyer" },
+                    { 2, 1, "4:26", "YYZ" },
+                    { 3, 2, "5:55", "Behemian Rhapsody" },
+                    { 4, 2, "2:52", "You're My Best Friend" },
+                    { 5, 3, "3:30", "All That She Wants" },
+                    { 6, 3, "3:08", "The Sign" },
+                    { 7, 4, "3:47", "Sell Out" },
+                    { 8, 4, "3:30", "Beer" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Albums_ArtistId",
+                table: "Albums",
+                column: "ArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_AlbumId",
+                table: "Songs",
+                column: "AlbumId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "Albums");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+        }
+    }
+}
