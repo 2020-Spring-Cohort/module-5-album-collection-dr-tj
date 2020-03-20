5c65f3a6cba165aaa6837131271d378e6be1c719
Dan Rasmussen <drasmussen49@gmail.com>
2020-03-20 11:25:04 -0400

    Added create button with POST action to Artist component

--- frontend/src/js/components/Artists.js
+++ frontend/src/js/components/Artists.js
@@ -4,8 +4,15 @@ export default function Artists(artists) {
             ${artists.map(artist => {
                 return `
                     <h5>${artist.name}</h5>
+                    
                 `
             }).join("")}
         </ul>
+
+    <section class="add-artist">
+        <input class="add-artist__artist-name" type="text" placeholder="Add an artist name">
+        <input class="add-artist__artist-genre" type="text" placeholder="Add a genre">
+        <button class="add-artist__submit">Add an artist</button>
+    </section>
     `
 }
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -8,7 +8,7 @@ import apiActions from "./api/apiActions";
 
 export default pageBuild;
 
-function pageBuild(){
+function pageBuild() {
     sidebar();
     footer();
     header();
@@ -25,21 +25,43 @@ function sidebar() {
     sidebar.innerHTML = Sidebar();
 }
 
-function footer(){
+function footer() {
     const footer = document.querySelector("#footer");
     footer.innerHTML = Footer();
 }
 
-function navArtists(){
+function navArtists() {
     const artistsNavButton = document.querySelector(".nav_artists");
     const mainDiv = document.querySelector(".main-div");
-    
-    artistsNavButton.addEventListener("click", function(){
+
+    artistsNavButton.addEventListener("click", function () {
         apiActions.getRequest("https://localhost:44313/api/Artist",
-        artists => {
-            console.log(artists);
-            mainDiv.innerHTML = Artists(artists);
-        }
+            artists => {
+                console.log(artists);
+                mainDiv.innerHTML = Artists(artists);
+            }
         )
     });
+
+    mainDiv.addEventListener("click", function () {
+        if (event.target.classList.contains("add-artist__submit")) {
+            const artistName = event.target.parentElement.querySelector(".add-artist__artist-name").value;
+            const artistGenre = event.target.parentElement.querySelector(".add-artist__artist-genre").value;
+            console.log(artistName);
+
+            var requestBody = {
+                Name: artistName,
+                Genre: artistGenre
+            }
+
+            apiActions.postRequest(
+                "https://localhost:44313/api/Artist",
+                requestBody,
+                artists => {
+                    console.log(artists);
+                    mainDiv.innerHTML = Artists(artists);
+                }
+            )
+        }
+    })
 }
