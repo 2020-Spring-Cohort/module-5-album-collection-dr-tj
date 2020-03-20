5944114e7134aa8bbfbe6387045e00c80f62a135
rhi-teetzel <rhiannateetzel@gmail.com>
2020-03-20 14:54:31 -0400

    setting up albums.js component

---
+++ frontend/src/js/components/Albums.js
@@ -0,0 +1,22 @@
+export default function Albums(albums) {
+    return `
+        <div class="album-grid-container">
+            ${albums.map(album => {
+                return `
+                    <div class="album-item-container">
+                        <h6 class="specific-album">${album.title}</h6>
+                        <button class="edit-album__submit">Edit</button>
+                        <button class="delete-album__submit">Delete</button>
+                        <input class="album-id" type="hidden" value="${album.id}">
+                    </div>
+                `
+            }).join("")}
+        </div>
+
+    <section class="add-album">
+        <input class="add-album__album-title" type="text" placeholder="Add an album title">
+        <input class="add-album__album-record-label" type="text" placeholder="Add a record label">
+        <button class="add-album__submit">Add an album</button>
+    </section>
+    `
+}
