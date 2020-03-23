bb8a395b0258805e42c07b60f215b19b66910697
Dan Rasmussen <drasmussen49@gmail.com>
2020-03-23 15:30:19 -0400

    Added Delete action for albums. Started working on dropdown menu to add artistId to album create

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -136,11 +136,37 @@ function navAlbums(){
         if(event.target.classList.contains("add-album__submit")){
             const albumTitle = event.target.parentElement.querySelector(".add-album__album-title").value;
             const albumRecordLabel = event.target.parentElement.querySelector(".add-album__album-record-label").value;
+            const albumArtistId = event.target.parentElement.querySelector(".add-album__artist-id").value;
 
-            console.log(albumTitle);
+            console.log(albumArtistId);
 
-            
+            var requestBody = {
+                Title: albumTitle,
+                RecordLabel: albumRecordLabel,
+                ArtistId: albumArtistId
+            }
+            apiActions.postRequest(
+                "https://localhost:44313/api/Album",
+                requestBody,
+                albums => {
+                    console.log(albums);
+                    mainDiv.innerHTML = Albums(albums);
+                }
+            )
         }
+    });
 
-    })
+    mainDiv.addEventListener("click", function () {
+        if (event.target.classList.contains("delete-album__submit")) {
+            const albumId = event.target.parentElement.querySelector(".album-id").value;
+            console.log(albumId);
+
+            apiActions.deleteRequest(
+                `https://localhost:44313/api/Album/${albumId}`,
+                albums => {
+                    mainDiv.innerHTML = Albums(albums);
+                }
+            )
+        }
+    });
 }
