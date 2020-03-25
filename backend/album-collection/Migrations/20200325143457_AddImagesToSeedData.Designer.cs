ea29836c0a8e4e69d4038245f44bf495412a8dbf
Dan Rasmussen <drasmussen49@gmail.com>
2020-03-25 10:37:02 -0400

    added images to seed data

--- backend/album-collection/Migrations/20200325143457_AddImagesToSeedData.Designer.cs
+++ backend/album-collection/Migrations/20200325143457_AddImagesToSeedData.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using album_collection;
 
 namespace album_collection.Migrations
 {
     [DbContext(typeof(MusicApiContext))]
-    partial class MusicApiContextModelSnapshot : ModelSnapshot
+    [Migration("20200325143457_AddImagesToSeedData")]
+    partial class AddImagesToSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/album-collection/Migrations/20200325143457_AddImagesToSeedData.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace album_collection.Migrations
+{
+    public partial class AddImagesToSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "./doc/img/Movingpictures.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "./docs/img/QueenANightAtTheOpera.png");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "./docs/img/TheSign.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "./docs/img/Turntheradiooff.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "./docs/img/Rush.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "./docs/img/Queen.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "./docs/img/Aceofbase.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "./doc/img/Reelbigfish.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Image",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Image",
+                value: "");
+        }
+    }
+}
