c7c041aa0ce13790cf013c534457ab51c015f9d6
TShaw516 <TShaw516@gmail.com>
2020-03-25 13:27:33 -0400

    styling changes to nav bar

--- frontend/src/js/components/Sidebar.js
+++ frontend/src/js/components/Sidebar.js
@@ -1,10 +1,10 @@
 export default function Sidebar() {
     return `<nav class="nav_sidebar">
-        <div>
+        <div class="nav_container">
             <article class="nav_home">Home</article>
-            <article class="nav_artists">Artists</article>
-            <article class="nav_albums">Albums</article>
-            <article class="nav_songs">Songs</article>
+            <img class="nav_artists" src="./img/artist.jpg">
+            <img class="nav_albums" src="./img/album.jpg">
+            <img class="nav_songs" src="./img/song.jpg">
         </div>
     </nav>`
 }
