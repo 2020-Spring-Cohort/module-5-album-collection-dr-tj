7d5b30cff703ea9aa35a96c7ac33fe93d72837a9
Dan Rasmussen <drasmussen49@gmail.com>
2020-03-25 14:29:52 -0400

    Changed wording in home comp

--- frontend/src/js/components/Home.js
+++ frontend/src/js/components/Home.js
@@ -2,7 +2,7 @@ export default function Home(){
     return `
         <h3>Welcome to the Music Repository!</h2>
         <div>
-            <p>Click on a picture on the sidebar to the left to view all artists, albums, or songs.<p>
+            <p>Click on the sidebar to the left to view all artists, albums, or songs.<p>
         </div>
         <div>
              <p>You can create new items, and edit/delete current items of artists, albums, or songs on their respective pages.<p>
